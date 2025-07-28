33e26a6bf0560e1e126ea1edd555d550f2e545a8
acetux <110208567+acetux@users.noreply.github.com>
2025-07-28 10:06:49 +0200

    test rev 81

--- pkgs/by-name/sp/spotify/linux.nix
+++ pkgs/by-name/sp/spotify/linux.nix
@@ -52,14 +52,14 @@ let
   # If an update breaks things, one of those might have valuable info:
   # https://aur.archlinux.org/packages/spotify/
   # https://community.spotify.com/t5/Desktop-Linux
-  version = "1.2.59.514.g834e17d4";
+  version = "0.0.1-test";
   # To get the latest stable revision:
   # curl -H 'X-Ubuntu-Series: 16' 'https://api.snapcraft.io/api/v1/snaps/details/spotify?channel=stable' | jq '.download_url,.version,.last_updated'
   # To get general information:
   # curl -H 'Snap-Device-Series: 16' 'https://api.snapcraft.io/v2/snaps/info/spotify' | jq '.'
   # More examples of api usage:
   # https://github.com/canonical-websites/snapcraft.io/blob/master/webapp/publisher/snaps/views.py
-  rev = "86";
+  rev = "81";
 
   deps = [
     alsa-lib
